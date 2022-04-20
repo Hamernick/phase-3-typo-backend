a8fadeb84166ebb26304005c7d53cf3121c46e56
Caleb Hamernick <caleb.hamernick@gmail.com>
2022-04-20 10:21:40 -0400

    updated methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,37 @@ class ApplicationController < Sinatra::Base
     users.to_json
   end
 
+  get '/users/:id' do
+    user = User.find(params[:id])
+    user.to_json
+  end
+
+
+  post '/users' do
+    user = User.create(
+      name: params[:name],
+      email: params[:email],
+      bio: params[:bio],
+      picture: params[:picture]
+    )
+    user.to_json
+  end
+
+
+  post '/new_note' do
+    note = Note.create(
+      title: params[:title],
+      body: params[:body],
+      notebook_id: params[:notebook_id],
+      user_id: params[:user_id]
+    )
+    note.to_json
+  end
+
+  get '/login/:name' do
+    user = User.find_by(:name => params[:name])
+    user.to_json(include: { notes: {include: :notebook}})
+  end
+
+
 end
